a36d4979e7e0f2f3df1cc1ae8385380b1f2b66a3
ViacheslavKonashkov <slava.konashkov@gmail.com>
2022-08-12 19:34:51 +0300

    HW 9.1 redis-cluster

---
+++ README.md
@@ -0,0 +1,14 @@
+# TASK
+**Redis Cluster**
+
+Build master-slave redis cluster
+Try all eviction strategies
+Write a wrapper for Redis Client that implement probabilistic cache clearing
+
+## Setup
+
+```shell
+docker-compose up
+```
+
+![Redis cluster](/docs/redis-cluster.png?raw=true "Redis cluster")
---
+++ src/ProbabilisticCacheWrapper.php
@@ -0,0 +1,54 @@
+<?php
+
+class ProbabilisticCacheWrapper
+{
+    public const CACHE_KEY = 'arch6';
+
+    public const TTL_SEC = 30;
+
+    public const BETA = 1;
+
+    private $redis;
+
+    public function __construct()
+    {
+        $this->redis = new \Predis\Client(['host' => $_ENV['REDIS_HOST']]);
+    }
+
+    public function set($value)
+    {
+        $this->redis->set(self::CACHE_KEY, $value, 'EX', self::TTL_SEC);
+    }
+
+    public function get()
+    {
+        return $this->redis->get(self::CACHE_KEY);
+    }
+
+    public function ttl()
+    {
+        return $this->redis->ttl(self::CACHE_KEY);
+    }
+
+    public function xfetch()
+    {
+        $redisValue = $this->get();
+        $info = @unpack('Pdelta/Pexpire/Z*value', $redisValue);
+        $delta = @$info['delta'];
+        $expire = @$info['expire'];
+        $value = @unserialize($info['value']);
+
+        if (!$redisValue || time() - $delta * self::BETA * log(rand(0,1)) >= $expire) {
+            $start = time();
+            $db = new DbWrapper($_ENV['DATABASE_URL'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
+            $value = $db->getAll();
+            $delta = time() - $start;
+            $expire = time() + self::TTL_SEC;
+            $redisValue = pack('PPZ*', $delta, $expire, serialize($value));
+
+            $this->set($redisValue);
+        }
+
+        return $value;
+    }
+}
